732f97640bbe91e7f622f4e03317a3167fdcba56
Bruce <irabruce10@gmail.com>
2024-01-26 13:13:16 +0100

    list of freind

--- src/App.js
+++ src/App.js
@@ -43,15 +43,25 @@ function List({ data }) {
 }
 function Friend({ friend }) {
   return (
-    <div>
-      <ul>
-        <li>
-          <img src={friend.image} alt={friend.image} />
-          <p>{friend.name}</p>
-          <span>{friend.balance}</span>
-          <button>Select</button>
-        </li>
-      </ul>
-    </div>
+    <li>
+      <img src={friend.image} alt={friend.image} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} € {Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you € {Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
   );
 }
