0ea0653e7855227b9d3de3900a866a80dddac4d6
Bruce <irabruce10@gmail.com>
2024-01-26 12:55:39 +0100

    Initialize project using create React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <List data={initialFriends} />
+      </div>
     </div>
   );
 }
 
-export default App;
+function List({ data }) {
+  return (
+    <ul>
+      {data.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+      ;
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <div>
+      <ul>
+        <li>
+          <img src={friend.image} alt={friend.image} />
+          <p>{friend.name}</p>
+          <span>{friend.balance}</span>
+          <button>Select</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
